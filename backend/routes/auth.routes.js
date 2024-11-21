f2001d2c198284b9276078bcd142188593c76e16
marchissio <marko.d.milosevic77@gmail.com>
2024-11-21 14:27:20 +0100

    1- auth controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  login,
+  logout,
+  signup,
+  getMe,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
+
+router.get("/me", protectRoute, getMe);
 
 router.post("/signup", signup);
 
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,17 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
-console.log(process.env.MONGO_URI);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // middleware for parsing application/json (req.body)
+app.use(express.urlencoded({ extended: true })); // to parse form data (urlencoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
